ff8a4395f4d58e1e15e9e7df87c491ac1e7f5231
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-08-16 10:38:10 +0530

    adding code for MDS

--- src/jslink/persent-column/index.js
+++ src/jslink/persent-column/index.js
@@ -1,20 +1,41 @@
 import PersentColTemplate from './template.js';
+import {JSLink} from '../../namespace.js';
 
-(function () {
-  const persentageColumnRender = function (ctx) {
-    const persentColTemplate = new PersentColTemplate(window);
-    var fieldVal = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
-    var percentComplete = fieldVal.toString().replace(' ', '');
-    return persentColTemplate.renderAsString({ value: percentComplete });
-  };
+JSLink.PersentageColumn = JSLink.PersentageColumn || {};
 
-  let persentageColumn = {};
+JSLink.PersentageColumn.Functions = JSLink.PersentageColumn.Functions || {};
+JSLink.PersentageColumn.Functions.RenderColumn = JSLink.PersentageColumn.Functions.RenderColumn || function (ctx) {
+  const persentColTemplate = new PersentColTemplate(window);
+  var fieldVal = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+  var percentComplete = fieldVal.toString().replace(' ', '');
+  return persentColTemplate.renderAsString({ value: percentComplete });
+};
 
-  persentageColumn.Templates = {};
-  persentageColumn.Templates.Fields = {
-    'PercentComplete': { 'View': persentageColumnRender }
-  };
+JSLink.PersentageColumn.Template = JSLink.PersentageColumn.Template || {
+  Templates: {
+    Fields: {
+      'PercentComplete': { 'View': JSLink.PersentageColumn.Functions.RenderColumn }
+    }
+  }
+};
+
+JSLink.PersentageColumn.Functions.RenderTemplate = function () {
+  // eslint-disable-next-line no-undef
+  SPClientTemplates.TemplateManager.RegisterTemplateOverrides(JSLink.PersentageColumn.Template);
+};
+
+JSLink.PersentageColumn.Functions.MdsRegisterTemplate = function () {
+  // eslint-disable-next-line no-undef
+  const thisUrl = _spPageContextInfo.siteServerRelativeUrl + 'Site Liberary/persent-column.bundle.js';
+  JSLink.PersentageColumn.Functions.RenderTemplate();
 
   // eslint-disable-next-line no-undef
-  SPClientTemplates.TemplateManager.RegisterTemplateOverrides(persentageColumn);
-})();
+  RegisterModuleInit(thisUrl, JSLink.PersentageColumn.Functions.RenderTemplate);
+};
+
+// eslint-disable-next-line no-undef
+if (typeof _spPageContextInfo != 'undefined' && _spPageContextInfo != null) {
+  JSLink.PersentageColumn.Functions.MdsRegisterTemplate();
+} else {
+  JSLink.PersentageColumn.Functions.RenderTemplate();
+}
---
+++ src/namespace.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+Type.registerNamespace('SolutionNamespace');
+
+const SolutionNamespace = window.SolutionNamespace || {};
+
+SolutionNamespace.JSLink = SolutionNamespace.JSLink || {};
+
+export const JSLink = SolutionNamespace.JSLink;
